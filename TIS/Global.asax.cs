44cc1fd4dc07a3830095831ec235f07b907ee906
KetanMevada <ketan.mevada@hotmail.com>
2026-02-15 21:28:19 +0530

    changes for CSP

--- TIS/Global.asax.cs
+++ TIS/Global.asax.cs
@@ -26,91 +26,45 @@ namespace TIS
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        //protected void Application_BeginRequest()
-        //{
-        //    Response.Headers.Add(
-        //        "Content-Security-Policy",
-        //        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; object-src 'none'; frame-ancestors 'self'; base-uri 'self'; form-action 'self';"
-        //    );
-        //}
-
-
-
         protected void Application_BeginRequest()
         {
-            // generate a per-request nonce for inline scripts
-            //var noncebytes = new byte[16];
-            //using (var rng = new RNGCryptoServiceProvider())
-            //{
-            //    rng.GetBytes(noncebytes);
-            //}
-
-            //var nonce = Convert.ToBase64String(noncebytes);
-            //HttpContext.Current.Items["cspnonce"] = nonce;
-
-            // fixed spacing and allow the remote host that serves the chunk
-
-
-
-
             var csp = $"default-src 'self'; " +
-                      // $"script-src 'self' https://static.hsappstatic.net 'nonce-{nonce}'; " +
-                      //$"script-src 'self' https://static.hsappstatic.net 'nonce-{nonce}' 'unsafe-eval';" +
-                      //$"script-src 'self' https://static.hsappstatic.net 'nonce-{nonce}' 'unsafe-eval';" +
                       $"script-src 'self' https://static.hsappstatic.net  'unsafe-eval' ;" +
                       $"style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; " +
                       $"font-src 'self' data: https://fonts.gstatic.com https://static.hsappstatic.net; " +
                       $"img-src 'self' data:; base-uri 'self'; frame-ancestors 'none'; form-action 'self';";
 
-            //var csp = $"script-src 'self' https://static.hsappstatic.net 'nonce-{nonce}' 'unsafe-eval';";
-
-            //if (Response.Headers.AllKeys.Contains("content-security-policy"))
-            //{
-            //    Response.Headers.Set("content-security-policy", csp);
-            //}
-            //else
-            //{
             Response.Headers.Add("content-security-policy", csp);
             HttpContext.Current.Response.Headers.Add("X-Content-Type-Options", "nosniff");
             Response.Headers.Remove("X-Powered-By");
             Response.Headers.Remove("X-AspNet-Version");
-            //}
         }
 
-        //protected void Application_BeginRequest()
-        //{
-        //    // generate a per-request nonce
-        //    var noncebytes = new byte[16];
-        //    using (var rng = new RNGCryptoServiceProvider())
-        //    {
-        //        rng.GetBytes(noncebytes);
-        //    }
+        // Ensure cookies set by application have Secure, HttpOnly and SameSite flags
+        protected void Application_EndRequest()
+        {
+            try
+            {
+                var cookies = Response.Cookies;
+                for (int i = 0; i < cookies.Count; i++)
+                {
+                    var cookie = cookies[i];
+                    if (cookie == null) continue;
 
-        //    var nonce = Convert.ToBase64String(noncebytes);
-        //    HttpContext.Current.Items["cspnonce"] = nonce;
+                    // Mark cookie HttpOnly and Secure
+                    cookie.HttpOnly = true;
+                    cookie.Secure = true; // requires HTTPS
 
-        //    // CSP with HubSpot, inline scripts, and eval support
-        //    var csp = $"default-src 'self'; " +
-        //              $"script-src 'self' https://static.hsappstatic.net http://static.hsappstatic.net 'nonce-{nonce}' 'unsafe-eval' 'unsafe-inline'; " +
-        //              $"style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; " +
-        //              $"font-src 'self' data: https://fonts.gstatic.com https://static.hsappstatic.net; " +
-        //              $"img-src 'self' data:; base-uri 'self'; frame-ancestors 'none'; form-action 'self';";
-
-        //    if (Response.Headers.AllKeys.Contains("content-security-policy"))
-        //        Response.Headers.Set("content-security-policy", csp);
-        //    else
-        //        Response.Headers.Add("content-security-policy", csp);
-        //}
-
-
-
-
-
-
-
-
-
-
+                    // Set SameSite (Strict). Adjust to Lax/None if your app requires cross-site usage.
+                    // If you set SameSite=None, Secure must be true by spec.
+                    cookie.SameSite = System.Web.SameSiteMode.Strict;
+                }
+            }
+            catch (Exception)
+            {
+                // swallowing errors here to avoid breaking responses; log if needed
+            }
+        }
 
     }
 }
